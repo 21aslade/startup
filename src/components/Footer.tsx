b5a18b0e9551e5a594c7740ed98272122088b22c
21aslade <andrewmslade@gmail.com>
2024-10-12 16:46:41 -0600

    fix footer spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,9 +12,9 @@ const Container = styled.footer`
 export default function Footer() {
     return (<Container>
         <p>
-            LineBreak was created by
+            LineBreak was created by&nbsp;
             <a href="https://github.com/21aslade">Andrew Slade</a> for BYU's CS 260.
-            View the source code on
+            View the source code on&nbsp;
             <a href="https://github.com/21aslade/startup">GitHub</a>
         </p>
     </Container>);
