97c618cfe00052c9e7256c5515f0014d54e98c99
21aslade <andrewmslade@gmail.com>
2024-11-05 18:39:03 -0700

    add icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,213 @@
+// Icons sourced from https://www.svgrepo.com
+const iconSize = "20px";
+const fill = "#ffffff";
+
+export function Play() {
+    return (
+        <svg
+            width={iconSize}
+            height={iconSize}
+            viewBox="-0.5 0 7 7"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g
+                id="Page-1"
+                stroke="none"
+                stroke-width="1"
+                fill="none"
+                fill-rule="evenodd"
+            >
+                <g
+                    id="Dribbble-Light-Preview"
+                    transform="translate(-347.000000, -3766.000000)"
+                    fill={fill}
+                >
+                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                        <path
+                            d="M296.494737,3608.57322 L292.500752,3606.14219 C291.83208,3605.73542 291,3606.25002 291,3607.06891 L291,3611.93095 C291,3612.7509 291.83208,3613.26444 292.500752,3612.85767 L296.494737,3610.42771 C297.168421,3610.01774 297.168421,3608.98319 296.494737,3608.57322"
+                            id="play-[#1003]"
+                        ></path>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    );
+}
+
+export function Pause() {
+    return (
+        <svg
+            width={iconSize}
+            height={iconSize}
+            viewBox="-1 0 8 8"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g
+                id="Page-1"
+                stroke="none"
+                stroke-width="1"
+                fill="none"
+                fill-rule="evenodd"
+            >
+                <g
+                    id="Dribbble-Light-Preview"
+                    transform="translate(-227.000000, -3765.000000)"
+                    fill={fill}
+                >
+                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                        <path
+                            d="M172,3605 C171.448,3605 171,3605.448 171,3606 L171,3612 C171,3612.552 171.448,3613 172,3613 C172.552,3613 173,3612.552 173,3612 L173,3606 C173,3605.448 172.552,3605 172,3605 M177,3606 L177,3612 C177,3612.552 176.552,3613 176,3613 C175.448,3613 175,3612.552 175,3612 L175,3606 C175,3605.448 175.448,3605 176,3605 C176.552,3605 177,3605.448 177,3606"
+                            id="pause-[#1006]"
+                        ></path>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    );
+}
+
+export function StepBack() {
+    return (
+        <svg
+            width={iconSize}
+            height={iconSize}
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            fill={fill}
+        >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M1.75 5.75v-4h1.5v2.542c1.145-1.359 2.911-2.209 4.84-2.209 3.177 0 5.92 2.307 6.16 5.398l.02.269h-1.5l-.022-.226c-.212-2.195-2.202-3.94-4.656-3.94-1.736 0-3.244.875-4.05 2.166h2.83v1.5H2.707l-.961-.975V5.75h.003zM8 14a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+            />
+        </svg>
+    );
+}
+
+export function StepInto() {
+    return (
+        <svg
+            width={iconSize}
+            height={iconSize}
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            fill={fill}
+        >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8 9.532h.542l3.905-3.905-1.061-1.06-2.637 2.61V1H7.251v6.177l-2.637-2.61-1.061 1.06 3.905 3.905H8zm1.956 3.481a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"
+            />
+        </svg>
+    );
+}
+
+export function StepOut() {
+    return (
+        <svg
+            width={iconSize}
+            height={iconSize}
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            fill={fill}
+        >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8 1h-.542L3.553 4.905l1.061 1.06 2.637-2.61v6.177h1.498V3.355l2.637 2.61 1.061-1.06L8.542 1H8zm1.956 12.013a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"
+            />
+        </svg>
+    );
+}
+
+export function StepOver() {
+    return (
+        <svg
+            width={iconSize}
+            height={iconSize}
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            fill={fill}
+        >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M14.25 5.75v-4h-1.5v2.542c-1.145-1.359-2.911-2.209-4.84-2.209-3.177 0-5.92 2.307-6.16 5.398l-.02.269h1.501l.022-.226c.212-2.195 2.202-3.94 4.656-3.94 1.736 0 3.244.875 4.05 2.166h-2.83v1.5h4.163l.962-.975V5.75h-.004zM8 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
+            />
+        </svg>
+    );
+}
+
+export function Skip() {
+    return (
+        <svg
+            width={iconSize}
+            height={iconSize}
+            viewBox="0 -2 12 12"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <title>next [#998]</title>
+            <desc>Created with Sketch.</desc>
+            <defs></defs>
+            <g
+                id="Page-1"
+                stroke="none"
+                stroke-width="1"
+                fill="none"
+                fill-rule="evenodd"
+            >
+                <g
+                    id="Dribbble-Light-Preview"
+                    transform="translate(-144.000000, -3805.000000)"
+                    fill={fill}
+                >
+                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                        <path
+                            d="M99.684,3649.69353 L95.207,3652.82453 C94.571,3653.25353 94,3652.84553 94,3652.13153 L94,3650.14053 L89.78,3652.82453 C89.145,3653.25353 88,3652.84553 88,3652.13153 L88,3645.86853 C88,3645.15453 89.145,3644.74653 89.78,3645.17453 L94,3647.85953 L94,3645.86853 C94,3645.15453 94.571,3644.74653 95.207,3645.17453 L99.516,3648.30653 C100.03,3648.65353 100.198,3649.34653 99.684,3649.69353"
+                            id="next-[#998]"
+                        ></path>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    );
+}
+
+export function SkipBack() {
+    return (
+        <svg
+            width={iconSize}
+            height={iconSize}
+            viewBox="0 -2 12 12"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <title>previous [#999]</title>
+            <desc>Created with Sketch.</desc>
+            <defs></defs>
+            <g
+                id="Page-1"
+                stroke="none"
+                stroke-width="1"
+                fill="none"
+                fill-rule="evenodd"
+            >
+                <g
+                    id="Dribbble-Light-Preview"
+                    transform="translate(-104.000000, -3805.000000)"
+                    fill={fill}
+                >
+                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                        <path
+                            d="M59.9990013,3645.86816 L59.9990013,3652.13116 C59.9990013,3652.84516 58.8540013,3653.25316 58.2180013,3652.82516 L53.9990013,3650.14016 L53.9990013,3652.13116 C53.9990013,3652.84516 53.4260013,3653.25316 52.7900013,3652.82516 L48.4790013,3649.69316 C47.9650013,3649.34616 47.7980013,3648.65316 48.3120013,3648.30616 L52.7900013,3645.17516 C53.4260013,3644.74616 53.9990013,3645.15416 53.9990013,3645.86816 L53.9990013,3647.85916 L58.2180013,3645.17516 C58.8540013,3644.74616 59.9990013,3645.15416 59.9990013,3645.86816"
+                            id="previous-[#999]"
+                        ></path>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    );
+}
