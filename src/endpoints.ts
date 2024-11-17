a8e0a1db9d3f03dfea00be58c8e8ce0bc8e7467d
21aslade <andrewmslade@gmail.com>
2024-11-16 21:59:11 -0700

    add wrapper functions for endpoints

---
+++ src/endpoints.ts
@@ -0,0 +1,117 @@
+import {
+    AuthToken,
+    isAuthToken,
+    isProfile,
+    Profile,
+    UserCredentials,
+} from "linebreak-service";
+
+export async function createUser(
+    credentials: UserCredentials
+): Promise<AuthToken> {
+    const result = await fetch("/api/user", {
+        method: "POST",
+        body: JSON.stringify(credentials),
+    });
+
+    return handleResponse(result, isAuthToken);
+}
+
+export async function deleteUser(
+    token: AuthToken,
+    user: string
+): Promise<void> {
+    const uriUser = encodeURI(user);
+    const result = await fetch(`/api/user/${uriUser}`, {
+        method: "DELETE",
+        body: JSON.stringify(token),
+    });
+
+    if (!result.ok) {
+        const body: unknown = await result.text();
+        throw new Error(`Request failed: code ${result.status} body ${body}`);
+    }
+}
+
+export async function getProfile(user: string): Promise<Profile> {
+    const uriUser = encodeURI(user);
+    const result = await fetch(`/api/user/${uriUser}`);
+
+    return handleResponse(result, isProfile);
+}
+
+export async function friendRequest(
+    auth: AuthToken,
+    user: string,
+    other: string
+): Promise<void> {
+    const uriUser = encodeURI(user);
+    const uriOther = encodeURI(other);
+    const result = await fetch(`/api/user/${uriUser}/friend/${uriOther}`, {
+        method: "PUT",
+        body: JSON.stringify(auth),
+    });
+
+    if (!result.ok) {
+        const body: unknown = await result.text();
+        throw new Error(`Request failed: code ${result.status} body ${body}`);
+    }
+}
+
+export async function unfriend(
+    auth: AuthToken,
+    user: string,
+    other: string
+): Promise<void> {
+    const uriUser = encodeURI(user);
+    const uriOther = encodeURI(other);
+    const result = await fetch(`/api/user/${uriUser}/friend/${uriOther}`, {
+        method: "DELETE",
+        body: JSON.stringify(auth),
+    });
+
+    if (!result.ok) {
+        const body: unknown = await result.text();
+        throw new Error(`Request failed: code ${result.status} body ${body}`);
+    }
+}
+
+export async function login(credentials: UserCredentials): Promise<AuthToken> {
+    const result = await fetch("/api/session", {
+        method: "POST",
+        body: JSON.stringify(credentials),
+    });
+
+    return handleResponse(result, isAuthToken);
+}
+
+export async function logout(token: AuthToken): Promise<void> {
+    const result = await fetch("/api/session", {
+        method: "DELETE",
+        body: JSON.stringify(token),
+    });
+
+    if (!result.ok) {
+        const body: unknown = await result.text();
+        throw new Error(`Request failed: code ${result.status} body ${body}`);
+    }
+}
+
+async function handleResponse<T>(
+    res: Response,
+    guard: (o: unknown) => o is T
+): Promise<T> {
+    try {
+        const body = await res.json();
+        if (res.ok && guard(body)) {
+            return body;
+        } else {
+            const bodyText = JSON.stringify(body, null, 2);
+            throw new Error(
+                `Request failed: code ${res.status} body ${bodyText}`
+            );
+        }
+    } catch (e) {
+        throw new Error(`Request failed: code ${res.status}`);
+    }
+}
